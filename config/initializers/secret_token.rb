4293b0b53c9ab91befbaec894c83184d668c2b0a
jamauro <jamauro@gmail.com>
2014-01-26 15:43:54 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hypem::Application.config.secret_token = '81d5f40c83b7365b5e47248a76e3a432ed2ae4c35ec3da7d4a0dd4443504b93ad61e63ed4ab63c28c4b410ba5e35c024ec4a2670e9e4affd65afd5b900cc13d9'
